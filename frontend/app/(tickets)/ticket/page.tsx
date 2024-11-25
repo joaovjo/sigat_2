2ac2b7589bb23695397f12ddf2fb4984284c04f3
João Vitor de Jesus Oliveira <contato@joaovjo.com.br>
2024-11-25 15:50:21 -0300

    initial commit

--- frontend/app/(tickets)/ticket/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TicketPage() {
-    return (
-        <div>
-            <h1>Ticket Page</h1>
-        </div>
-    )
-}
--- frontend/services/api/users.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@/types/users'
-
-export async function userService(): Promise<User[]> {
-    let api = await fetch(`${process.env.API_URL}/users`)
-    let userData = await api.json()
-    return userData
-}
